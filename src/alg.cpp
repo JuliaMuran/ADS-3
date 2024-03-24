7a7b83715ed926e7aca7862cbaa497d05c4355a0
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-03-24 21:25:28 +0300

    3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,27 +1,25 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-    int count=0;
-    int left=0;
-    int right=size-1;
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
 
-    while (left<=right){
-        int midd=left+(right-left)/2;
-        if(arr[midd]<value){
-            left=midd+1;
-        }
-        else if (arr[midd]>value){
-            right=midd-1;
-        }
-        else if (arr[midd]==value){
+    while (left <= right){
+        int midd = left + (right - left) / 2;
+        if (arr[midd] < value) {
+            left = midd + 1;
+        } else if (arr[midd] > value) {
+            right = midd - 1;
+        } else if (arr[midd] == value){
             count++;
-            right=midd+1;
-            while (arr[right]==value){
+            right = midd + 1;
+            while (arr[right] == value) {
                 count++;
                 right++;
             }
-            left=midd-1;
-            while (arr[left]==value){
+            left = midd - 1;
+            while (arr[left] == value) {
                 count++;
                 left--;
             }
